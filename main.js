0b994d0f235684da33f176a2146e507519e19aa5
Stjepanslo <90937675+Stjepanslo@users.noreply.github.com>
2026-08-03 17:35:47 -0300

    Actualizacion main.js

--- main.js
+++ main.js
@@ -15,7 +15,9 @@ app.use(cors({
   origin: [
     "http://localhost:3000",
     "http://localhost:5173",
-    "https://parcial2-aplicaciones-hibridas2026.vercel.app"
+    "https://parcial2-aplicaciones-hibridas2026.vercel.app",
+    "https://parcial2-aplicaciones-hibridas2-git-e84301-stjepanslos-projects.vercel.app/",
+    "https://parcial2-aplicaciones-hibridas2026-3x2w265ve.vercel.app/"
   ],
   credentials: true
 }))
